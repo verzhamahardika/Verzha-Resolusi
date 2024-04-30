205a250c3400965546bd94d2e0500694de4f4254
Verzha Mahardika <93263031+verzhamahardika@users.noreply.github.com>
2024-04-30 20:51:40 +0800

    Add Code to use database
    This code are use to database

--- Mysql Tutorial #1 .sql
+++ Mysql Tutorial #1 .sql
@@ -1,3 +1,5 @@
 MYSQL are database management systems which open source to use.
 
 CREATE DATABASE MyDB; --this code for create database name is mydb--
+
+Use MyDB; --After you create the database you can use the database--
